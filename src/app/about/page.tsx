d42b8e489bcb12eff5efb0758a544572ee5c878a
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2024-12-17 19:58:29 +0530

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+    return (
+        <div>
+            <h1 className="text-white">This page shows about us </h1>
+            <p className="text-teal-300">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Perspiciatis odit saepe illo atque deleniti voluptas quas magnam
+                voluptates unde corrupti obcaecati omnis, tempore, velit error?
+                Culpa sapiente veniam minima incidunt quidem ipsum iusto, minus
+                repellendus deleniti aut ab nisi odio aliquid quas ullam
+                praesentium beatae fugiat labore nihil possimus est?
+            </p>
+        </div>
+    );
+}
